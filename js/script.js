f7bee1b02b7cffb210d6be8b406a3e84749a553a
NELIOS <sookhaal@gmail.com>
2014-01-22 21:26:52 +0100

    Fixes/Addition
    Fixed fu***ng firefox not working -_-
    Fixed scrolling
    Added multi files support

--- js/script.js
+++ js/script.js
@@ -1,10 +1,45 @@
-$("#content1, #content2, #content3").hide();
-$( function() {
-    $( 'audio' ).audioPlayer();
-});
-
 var bContent1 = false, bContent2 = false, bContent3 = false;
-var widthValue, paddingValue;
+var widthValue, paddingValue, FIREFOX;
+
+jQuery.fn.center = function () {
+    this.css("position", "absolute");
+    this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) + 
+                                                $(window).scrollTop()) + "px");
+    this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) + 
+                                                $(window).scrollLeft()) + "px");
+    return this;
+}
+
+function preload(images) {
+    if (document.images) {
+        var i = 0;
+        var imageArray = new Array();
+        imageArray = images.split(',');
+        var imageObj = new Image();
+        for(i=0; i<=imageArray.length-1; i++) {
+            imageObj.src=imageArray[i];
+        }
+    }
+}
+
+$("#content1, #content2, #content3").hide();
+
+$(function () {
+    "use strict";
+    $('audio').audioPlayer();
+
+    FIREFOX = /Firefox/i.test(navigator.userAgent);
+    if (FIREFOX) {
+        $(".left, .right").hide();
+        $("#center").css({
+            position: "absolute"
+        });
+        
+        console.log("Test");
+
+        $(".center").center();
+    }
+});
 
 $(".toggle1").click(function (e) {
     "use strict";
@@ -15,9 +50,11 @@ $(".toggle1").click(function (e) {
     
     $(".listen").toggleClass("hovered");
     
-    if (!bContent1) {
+    if (!bContent1 && !FIREFOX) {
         widthValue = 900;
         paddingValue = 50;
+    } else if (FIREFOX){
+        
     } else {
         widthValue = 500;
         paddingValue = 30;
