0a55ef6ec5d603d1217a8034c929e1ca0c689ef3
NELIOS <sookhaal@gmail.com>
2014-02-19 14:03:18 +0100

    Update
    Updated group buttons
    Updated colours

--- ronjtest/js/buttons.js
+++ ronjtest/js/buttons.js
@@ -2,68 +2,79 @@ var buttonSize;
 
 $(function(){
     positionDiv();
-    //colorize(1,6);
 });
 
 $(window).resize(function () {
     positionDiv();
 });
 
+//6
 $("#b61").click(function (e){
-    colorize(1,6);
+    colorize(12,5,"05cdff", "000");
+    colorize(6,11,"2b77de", "000");
 });
 $("#b62").click(function (e){
-    colorize(7,12);
+    colorize(1,6,"05cdff", "000");
+    colorize(7,12,"2b77de", "000");
 });
 $("#b63").click(function (e){
-    colorize(3,8);
+    colorize(2,7,"05cdff", "000");
+    colorize(8,1,"2b77de", "000");
 });
 $("#b64").click(function (e){
-    colorize(9,2);
+    colorize(3,8,"05cdff", "000");
+    colorize(9,2,"2b77de", "000");
 });
 $("#b65").click(function (e){
-    colorize(5,10);
+    colorize(4,9,"05cdff", "000");
+    colorize(10,3,"2b77de", "000");
 });
 $("#b66").click(function (e){
-    colorize(11,4);
+    colorize(5,10,"05cdff", "000");
+    colorize(11,4,"2b77de", "000");
 });
 
+//4
+$("#b41").click(function (e){
+    colorize(12,3,"05cdff", "000");
+    colorize(4,7,"ffb005", "000");
+    colorize(8,11,"c1ff05", "000");
+});
+$("#b42").click(function (e){
+    colorize(1,4,"05cdff", "000");
+    colorize(5,8,"ffb005", "000");
+    colorize(9,12,"c1ff05", "000");
+});
+$("#b43").click(function (e){
+    colorize(2,5,"05cdff", "000");
+    colorize(6,9,"ffb005", "000");
+    colorize(10,1,"c1ff05", "000");
+});
+$("#b44").click(function (e){
+    colorize(3,6,"05cdff", "000");
+    colorize(7,10,"ffb005", "000");
+    colorize(11,2,"c1ff05", "000");
+});
+
+//3
 $("#b31").click(function (e){
-    colorize(12,2);
+    colorize(12,2,"f646fd", "000");
+    colorize(3,5,"05cdff", "000");
+    colorize(6,8,"ffb005", "000");
+    colorize(9,11,"c1ff05", "000");
 });
 $("#b32").click(function (e){
-    colorize(3,5);
+    colorize(1,3,"f646fd", "000");
+    colorize(4,6,"05cdff", "000");
+    colorize(8,9,"ffb005", "000");
+    colorize(10,12,"c1ff05", "000");
 });
 $("#b33").click(function (e){
-    colorize(6,8);
-});
-$("#b34").click(function (e){
-    colorize(9,11);
-});
-$("#b35").click(function (e){
-    colorize(1,3);
-});
-$("#b36").click(function (e){
-    colorize(4,6);
-});
-$("#b37").click(function (e){
-    colorize(7,9);
-});
-$("#b38").click(function (e){
-    colorize(10,12);
-});
-$("#b39").click(function (e){
-    colorize(2,4);
-});
-$("#b310").click(function (e){
-    colorize(5,7);
-});
-$("#b311").click(function (e){
-    colorize(8,10);
-});
-$("#b312").click(function (e){
-    colorize(11,1);
-});
+    colorize(2,4,"f646fd", "000");
+    colorize(5,7,"05cdff", "000");
+    colorize(7,10,"ffb005", "000");
+    colorize(11,1,"c1ff05", "000");
+})
 
 function positionDiv(){
     $("#groupbuttons").css({
@@ -80,31 +91,25 @@ function positionDiv(){
     });
 }
 
-function colorize(from, to){
-    for (var i = 0; i <= 11; i++){
-        $("#"+i).css({
-            "background-color":"#13171c",
-            "color":"#eee"
-        });
-    }
+function colorize(from, to, bgcolor, textcolor){
     if (from-to <0){
         for (var i = from-1; i <= to-1; i++){
             $("#"+i).css({
-                "background-color":"#05cdff",
-                "color":"#000"
+                "background-color":"#"+bgcolor,
+                "color":"#"+textcolor
             });
         }
     } else {
         for (var i = from-1; i <= 11; i++){
             $("#"+i).css({
-                "background-color":"#05cdff",
-                "color":"#000"
+                "background-color":"#"+bgcolor,
+                "color":"#"+textcolor
             });
         }
         for (var i = to-1; i >= 0; i--){
             $("#"+i).css({
-                "background-color":"#05cdff",
-                "color":"#000"
+                "background-color":"#"+bgcolor,
+                "color":"#"+textcolor
             });
         }
     }
